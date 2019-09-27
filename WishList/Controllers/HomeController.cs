8cb19d635ed8f4ce712c3901e665bd4eb62faffb
Mazin Babo <mazin-b@hotmail.com>
2019-09-27 12:00:55 +0100

    Completed module

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WishList.Controller
+namespace WishList.Controllers
 {
     public class HomeController : Controller
     {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
 
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
     }
 }
